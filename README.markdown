4dc58bcdd2532feaa589ad32d8df13df799ca0f8
Ken  Poland <ken.poland@gmail.com>
2011-12-29 12:14:50 +0000

    improve

--- README.markdown
+++ README.markdown
@@ -1,4 +1,4 @@
-== Welcome to Rails
+== 1st app Rails
 
 Rails is a web-application framework that includes everything needed to create
 database-backed web applications according to the Model-View-Control pattern.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '835555390020f4170708d3d1924c6ffaa76c0c35e74fb03845b33bcd989d9400760c610ee2b67b3db0161b6c83a2dc62b5526fcb70c1953aa4499478a0b9d55e'
